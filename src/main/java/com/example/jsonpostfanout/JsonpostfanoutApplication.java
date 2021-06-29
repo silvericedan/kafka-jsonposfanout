a7b7095c49f553f9929851987cf601ebd2351978
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-06-29 10:46:46 -0300

    initial commit

---
+++ src/main/java/com/example/jsonpostfanout/JsonpostfanoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonpostfanout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonpostfanoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonpostfanoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsonpostfanout/JsonpostfanoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonpostfanout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonpostfanoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
